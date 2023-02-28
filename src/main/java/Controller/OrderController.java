be0db396bf5e7025685f467148f29fbde3b063db
taeyeong <taeyeong30@gmail.com>
2023-02-28 13:52:08 +0900

    order 컨트롤러, 다오, 브이오 추가
    2023 02 28 13:52 정태영

---
+++ src/main/java/Controller/OrderController.java
@@ -0,0 +1,72 @@
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.rowset.serial.SerialException;
+
+import DAO.MemberDAO;
+import VO.MemberVO;
+
+@WebServlet("/order1/*") 
+public class OrderController extends HttpServlet {
+
+	MemberDAO memberdao;
+	
+	@Override
+	public void init() throws ServletException {
+		memberdao = new MemberDAO();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, 
+						 HttpServletResponse response) 
+						 throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, 
+						 HttpServletResponse response) 
+						 throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doHandle(HttpServletRequest request, 
+			HttpServletResponse response) 
+			 throws ServletException, IOException {
+		
+		//한글처리
+		request.setCharacterEncoding("UTF-8");
+		
+		//웹브라우저로 응답할 데이터 종류 설정
+		response.setContentType("text/html; charset=utf-8");
+		
+		//웹브라우저와 연결된 출력 스트림 통로 만들기
+		PrintWriter out = response.getWriter();
+		
+		//서블릿으로 요청한 주소를 request에서 얻기
+		String action = request.getPathInfo();
+		System.out.println("요청한 주소 : " + action);
+		
+		//조건에 따라서  포워딩 또는 보여줄 VIEW주소 경로를 저장할 변수 
+		String nextPage = null;
+		// 요청한 중앙화면  뷰 주소를 저장할 변수 
+		String center = null;
+		
+		
+			
+		
+		//포워딩 (디스패처 방식)
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		
+		dispatch.forward(request, response);
+	}
+	
+}
