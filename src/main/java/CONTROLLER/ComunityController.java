a459013f503d9c1bd05edf1eee65fbbaf7d183a5
taeyeong <taeyeong30@gmail.com>
2023-03-09 16:43:06 +0900

    게시판 컨트롤러, 다오, 브이오 추가

---
+++ src/main/java/CONTROLLER/ComunityController.java
@@ -0,0 +1,93 @@
+package CONTROLLER;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.ComunityDAO;
+
+
+//http://localhost:8090/greaitProject/com/Main 
+
+@WebServlet("/com/*")
+public class ComunityController extends HttpServlet {
+
+	//CarDAO객체를 저장할 참조변수 선언
+	ComunityDAO cardao;
+	//MemberDAO객체를 저장할 참조변수 선언  ----
+	
+	@Override
+	public void init() throws ServletException {
+		cardao = new ComunityDAO();
+	}
+		
+	@Override
+	protected void doGet(HttpServletRequest request, 
+						 HttpServletResponse response) 
+						 throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, 
+						 HttpServletResponse response) 
+						 throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doHandle(HttpServletRequest request, 
+							 HttpServletResponse response) 
+							 throws ServletException, IOException {
+		
+		//클라이언트의 웹브라우저로 응답할 데이터 종류 설정 및 문자처리방식 UTF-8설정 
+		response.setContentType("text/html; charset=utf-8");
+		
+		//1.한글처리 
+		request.setCharacterEncoding("UTF-8");
+		//1.1 클라이언트가 요청한 2단계요청주소 request객체에서 얻기
+		String action = request.getPathInfo();
+		System.out.println("2단계 요청 주소 : " + action);
+		
+		String nextPage = null;
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("nickname", "admin");
+		
+		if(action.equals("/Main")) {
+			
+			
+			nextPage = "/main.jsp";
+		}
+		
+	
+		//포워딩 (디스패처 방식)
+		RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+		dispatcher.forward(request, response);
+	
+	}//doHandle()메소드 끝 
+}//클래스 끝
+
+
+
+
+
+
+
+
