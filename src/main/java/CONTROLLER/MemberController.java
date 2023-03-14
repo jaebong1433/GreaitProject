4e370f1f78077d9073fd6b3b306d3b8d836afc75
hu <isv44@naver.com>
2023-03-14 09:09:28 +0900

    123

---
+++ src/main/java/CONTROLLER/MemberController.java
@@ -0,0 +1,403 @@
+package CONTROLLER;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.rowset.serial.SerialException;
+
+import DAO.MemberDAO;
+import VO.MemberVO;
+
+@WebServlet("/member1/*") 
+public class MemberController extends HttpServlet {
+	//멤버 서비스 추가
+	
+	MemberDAO memberdao;
+	
+	@Override
+	public void init() throws ServletException {
+		memberdao = new MemberDAO();
+		//멤버 서비스 
+		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, 
+						 HttpServletResponse response) 
+						 throws ServletException, IOException {
+		
+		doHandle(request, response);
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, 
+						 HttpServletResponse response) 
+						 throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doHandle(HttpServletRequest request, 
+			HttpServletResponse response) 
+			 throws ServletException, IOException {
+		
+		//한글처리
+		request.setCharacterEncoding("UTF-8");
+		
+		//웹브라우저로 응답할 데이터 종류 설정
+		response.setContentType("text/html; charset=utf-8");
+		
+		//웹브라우저와 연결된 출력 스트림 통로 만들기
+		PrintWriter out = response.getWriter();
+		
+		//서블릿으로 요청한 주소를 request에서 얻기
+		String action = request.getPathInfo();
+		System.out.println("요청한 주소 : " + action);
+		
+		//조건에 따라서  포워딩 또는 보여줄 VIEW주소 경로를 저장할 변수 
+		String nextPage = null;
+		// 요청한 중앙화면  뷰 주소를 저장할 변수 
+		String center = null;
+		
+		//메인화면 요청주소
+		if(action.equals("/main.me")) {
+			center = request.getParameter("center"); 
+			request.setAttribute("center", center);
+			nextPage = "/main.jsp";
+			
+			
+			//회원가입 닉네임 얻기
+		}else if (action.equals("/joinnicknameCheck.me")) {
+			//입력한 닉네임 얻기
+			String m_nickname = request.getParameter("nickname");
+			
+			boolean result = memberdao.overlappedNickname(m_nickname);
+			
+			if(result == true) {
+				out.write("not_usable");
+				return;
+			}else {
+				out.write("usable");
+				return;
+			}
+				
+		}
+		//회원가입 아이디 얻기
+		else if(action.equals("/joinIdCheck.me")) {
+			//입력한 아이디 얻기
+			String m_id = request.getParameter("m_id");
+			
+
+			//입력한 아이디가 DB에 저장되어 있는지 중복 체크 작업 
+			//true -> 중복 , false -> 중복아님  둘중 하나를 반환 받음
+			boolean result = memberdao.overlappedId(m_id);
+			
+			//아이디 중복결과를 다시 한번 확인 하여 조건값을 
+			//join.jsp파일과 연결된 join.js파일에 작성해 놓은
+			//success:function의 data매개변수로 웹브라우저를 거쳐 보냅니다!
+			if(result == true) {
+				out.write("not_usable");
+				return;
+			}else {
+				out.write("usable");
+				return;
+			}
+			
+			
+			}
+			//회원가입 실행 
+			else if(action.equals("/joinPro.me")) {
+				//요청한 값 얻기
+				String m_nickname = request.getParameter("m_nickname");
+				String m_id = request.getParameter("m_id");
+				String m_pw = request.getParameter("m_pw");
+				String m_name = request.getParameter("name");
+				String m_email = request.getParameter("m_email");
+			
+				
+				MemberVO vo = new MemberVO(
+											m_nickname,
+											m_id,
+											m_pw,
+											m_name,
+											m_email);
+				
+				memberdao.insertMember(vo);
+
+				nextPage="/main.jsp";
+
+				
+			}
+			//로그인 수행
+			else if(action.equals("/login.me")) {//로그인 창으로 이동
+			
+				center = request.getParameter("center");
+			
+				//중앙화면 주소 바인딩
+
+				request.setAttribute("center", center);
+			
+				//전체 메인화면 주소 저장
+
+				nextPage = "/main.jsp";
+				
+				
+			}
+			//로그인 유효성 
+			else if(action.equals("/loginPro.me")) {//로그인 수행
+				String m_id = request.getParameter("m_id");
+				String m_pw = request.getParameter("m_pw");
+			
+				int check = memberdao.loginCheck(m_id, m_pw);
+
+				if(check == 0) {//아이디는 맞고 비번 틀림
+					out.println("<script>");
+					out.println(" window.alert('비밀번호 틀림');");
+					out.println(" history.go(-1);");
+					out.println("</script>");
+					return;
+				}else if(check == -1) {//아이디 틀림 , 비밀번호 맞음 	
+					out.println("<script>");
+					out.println(" window.alert('아이디 틀림');");
+					out.println(" history.back();");
+					out.println("</script>");
+					return;				
+				}
+				
+				//session메모리생성
+				HttpSession session = request.getSession();
+				
+				//session메모리에 입력한 아이디 바인딩(저장)
+				session.setAttribute("m_id", m_id);
+				
+				//메인화면 VIEW 주소
+				nextPage = "/main.jsp";
+
+
+			}
+				
+			else if(action.equals("/logoutPro.me")) {//로그아웃 수행
+					//기존에 생성했던 session메모리 얻기
+				HttpSession session = request.getSession();
+				session.invalidate();
+		
+				nextPage = "/main.jsp";
+		
+				
+			}		
+			
+			else if (action.equals("/logout.me")) {//로그아웃
+					
+				HttpSession session = request.getSession();
+					
+				session.invalidate();
+							
+				nextPage = "/main.jsp";
+				
+				
+			}
+			// 아이디 찾기 완료 비밀번호 찾기
+			else if(action.equals("/findId.me")) {//아이디 찾기
+					
+					
+				request.setAttribute("center","findId.jsp");
+					
+					
+				nextPage = "/main.jsp";
+				
+				
+			}
+				
+			else if(action.equals("/findIdResult.me")) {//아이디 찾기
+					
+					
+				String m_name = request.getParameter("n_name");
+				String m_email = request.getParameter("m_email");
+					
+				MemberVO vo = memberdao.findId(m_name,m_email);
+					
+				request.setAttribute("vo", vo);
+				
+				request.setAttribute("center","findIdResult.jsp");
+					
+//				if(check == 0) {//아이디는 맞고 비번 틀림
+//					out.println("<script>");
+//					out.println(" window.alert('비밀번호 틀림');");
+//					out.println(" history.go(-1);");
+//					out.println("</script>");
+//					return;
+//				}else if(check == -1) {//아이디 틀림 , 비밀번호 맞음 	
+//					out.println("<script>");
+//					out.println(" window.alert('아이디 틀림');");
+//					out.println(" history.back();");
+//					out.println("</script>");
+//					return;				
+//				}
+					
+				nextPage = "/main.jsp";
+					
+					
+			}
+		
+			else if (action.equals("/findPw.me")) {//비밀번호 찾기수행
+					
+					
+					request.setAttribute("center","findPw.jsp");
+					
+					nextPage = "/main.jsp";
+					
+					
+			}
+		
+			else if(action.equals("/findPwResult.me")) {//비밀번호 찾기
+					
+					
+				String m_name = request.getParameter("m_name");
+				String m_id= request.getParameter("m_id");
+				String m_email = request.getParameter("m_email");
+
+				MemberVO vo = memberdao.findPw(m_name,m_id,m_email);
+					
+				request.setAttribute("vo", vo);
+					
+				request.setAttribute("center","findPwResult.jsp");
+					
+//				if(check == 0) {//아이디는 맞고 비번 틀림
+//					out.println("<script>");
+//					out.println(" window.alert('비밀번호 틀림');");
+//					out.println(" history.go(-1);");
+//					out.println("</script>");
+//					return;
+//				}else if(check == -1) {//아이디 틀림 , 비밀번호 맞음 	
+//					out.println("<script>");
+//					out.println(" window.alert('아이디 틀림');");
+//					out.println(" history.back();");
+//					out.println("</script>");
+//					return;				
+//				}
+				nextPage = "/main.jsp";
+				
+				
+			}
+			//카카오 로그인창에서 로그인 버튼을 눌렀을 때
+			else if(action.equals("/kakaoLoginPro.me")) {
+				
+				//---부장
+				
+					
+				//메인화면 view 주소
+				nextPage = "/main.jsp";
+			}
+		
+			//회원정보 수정을 위해 회원정보 조회 요청!
+			else if(action.equals("/mypage.me")) { 
+				//요청한 값 얻기
+				String m_id = request.getParameter("m_id");
+					
+				MemberVO vo = memberdao.findMember(m_id);
+				
+				//View중앙화면에 보여주기 위해 request에  vo를 바인딩
+				request.setAttribute("vo", vo);
+	
+				nextPage = "/myPage.jsp";
+			}
+		
+			//회원정보 수정을 위해 회원정보 수정창 요청!
+			else if(action.equals("/mypageUpdate.me")) { 
+			
+				//요청한 값 얻기
+				String m_id = request.getParameter("m_id");
+			
+				MemberVO vo = memberdao.findMember(m_id);
+			
+				//View중앙화면에 보여주기 위해 request에  vo를 바인딩
+				request.setAttribute("vo", vo);
+			
+				nextPage = "/modMemberForm.jsp";
+			}
+
+			//회원정보 수정창에서 수정완료 버튼을 클릭했을 때
+			else if(action.equals("/update.me")) {
+				int result = memberdao.updateMember(request);
+				
+				if(result == 1) {//수정 성공
+					
+					
+				out.print("<script>" + "  alert('회원정보가 수정 되었습니다.');" 
+					                     + " location.href='" + request.getContextPath()
+										 + "/member1/main.me'"
+		                  + "</script>");
+
+				return;
+					
+				}else {
+					
+					out.print("<script>"
+							+ " alert('회원정보 수정 실패!');"
+							+ " history.back();"
+							+ "</script>");
+					return;
+				}
+				
+				
+			}
+			//회원탈퇴를 위해 비밀번호를 입력하는 화면 요청! 
+			else if(action.equals("/delete.do")) {
+			
+				nextPage = "/Delete.jsp";
+				
+				
+			}
+			//회원탈퇴 
+			else if(action.equals("/signOut.me")) {
+				//요청한 값 얻기
+				//삭제할 예약아이디, 삭제를 위해 입력한 비밀번호 
+				String m_id = request.getParameter("m_id")  ;
+				String m_pw = request.getParameter("m_pw");
+				
+				
+				//응답할 값 마련 
+				//예약정보를 삭제(취소)하기 위해 CarDAO객체의 OrderDelete메소드 호출할떄...
+				//매개변수로 삭제할 예약아이디와 입력한 비밀번호 전달하여 DB에서 DELETE시키자
+				//삭제에 성공하면 OrderDelete메소드의 반환값은 삭제에 성공한 레코드 개수 1을 반환 받고
+				//실패하면 0을 반환 받습니다.
+				int result = memberdao.MemberDelete(m_id,m_pw);
+				
+				System.out.println(result);
+				
+				if(result == 1) {//삭제 성공
+					
+					
+					out.print("<script>" 
+								+ "  alert('회원정보가 탈퇴되었습니다.');" 
+								+ " location.href='" + request.getContextPath()
+			                    + "/member1/logout.me'" 
+		                  + "</script>");
+	
+					return;
+					
+				}else {
+					
+					out.print("<script>"
+							+ " alert('회원정보 탈퇴 실패!');"
+							+ " history.back();"
+							+ "</script>");
+					return;
+				}
+				
+				
+			}		
+		
+			//포워딩 (디스패처 방식)
+			RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		
+			dispatch.forward(request, response);
+			}
+	
+		}
---
+++ src/main/java/DAO/MemberDAO.java
@@ -0,0 +1,381 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import VO.MemberVO;
+
+//DB와 연결하여 비즈니스로직 처리 하는 클래스 
+public class MemberDAO {
+	
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	DataSource ds;
+	
+	//커넥션풀 생성 후 커넥션 객체 얻는 생성자
+	public MemberDAO() {
+		//context.xml파일에 설정한
+		//Resource태그에 적힌  DataSource커넥션풀 객체 받아오기	
+		try {
+			//웹프로젝트의 디렉터리에 접근하기 위한 객체 생성
+			Context ctx = new InitialContext();
+			//커넥션풀 얻기 
+			ds = (DataSource)ctx.lookup("java:/comp/env/jdbc/oracle");
+		
+		} catch (Exception e) {
+			System.out.println("DB연결 실패! - " + e);
+		}	
+	}
+	
+	//자원해제 기능
+	private void closeResource() {
+		if(con != null)try {con.close();} catch (Exception e) {e.printStackTrace();}
+		if(pstmt != null)try {pstmt.close();} catch (Exception e) {e.printStackTrace();}
+		if(rs != null)try {rs.close();} catch (Exception e) {e.printStackTrace();}		
+	}	
+	
+	//닉네임 중복 체크 !
+		public boolean overlappedNickname(String m_nickname) {//입력한 닉네임를 매개변수 닉네임로 받는다
+			
+			boolean result = false;
+			
+			try {
+				
+				con = ds.getConnection();
+				
+				
+				String sql = "select decode(count(*),1,'true','false') ";
+					   sql	+= "as result from member";
+					   sql  += " where m_nickname=?";
+				
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, m_nickname);
+				
+				rs = pstmt.executeQuery();
+				
+				rs.next();//조회된 제목줄에 커서(화살표)가 있다가 조회된 줄로 내려가 위치함
+				
+				String value = rs.getString("result");//"true"
+				
+				//문자열 "true" 또는 "false"를   Boolean자료형으로 형변환 하여 저장
+				result = Boolean.parseBoolean(value);//true 또는 false
+							
+				//true -> 닉네임 중복
+				//false-> 닉네임이 DB에 없으므로 중복 아님 
+				
+			}catch(Exception e) {
+
+				System.out.println("overlappednickname 메소드 내부에서 오류");
+				e.printStackTrace();
+			}finally {
+				closeResource();//자원해제
+			}
+		
+			return result;//true 또는 false를  MemberController로 반환 
+		}
+		
+	
+	
+	
+	//아이디 중복 체크 !
+	public boolean overlappedId(String m_id) {//입력한 아이디를 매개변수 id로 받는다
+		
+		boolean result = false;
+		
+		try {
+			
+			con = ds.getConnection();
+			
+			//오라클의 decode()함수를 이용하여 서블릿에서 전달되는
+			//입력한 ID에 해당하는 데이터를 검색하여 true 또는 false를 반환하는데
+			//검색한 갯수가 1(검색한 레코드가 존재하면)이면 'true'를 반환,
+			//존재하지 않으면 'false'를 문자열로 반환하여 조회합니다.
+			String sql = "select decode(count(*),1,'true','false') ";
+				   sql	+= "as result from member";
+				   sql  += " where m_id=?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, m_id);
+			
+			rs = pstmt.executeQuery();
+			
+			rs.next();//조회된 제목줄에 커서(화살표)가 있다가 조회된 줄로 내려가 위치함
+			
+			String value = rs.getString("result");//"true"
+			
+			//문자열 "true" 또는 "false"를   Boolean자료형으로 형변환 하여 저장
+			result = Boolean.parseBoolean(value);//true 또는 false
+						
+			//true -> 아이디 중복
+			//false-> 아이디가 DB에 없으므로 중복 아님 
+			
+		}catch(Exception e) {
+
+			System.out.println("overlappedId 메소드 내부에서 오류");
+			e.printStackTrace();
+		}finally {
+			closeResource();//자원해제
+		}
+	
+		return result;//true 또는 false를  MemberController로 반환 
+	}
+	
+	//주소 업데이트 완료 
+	public void insertMember(MemberVO vo) {
+	
+		try {
+			//커넥션 풀에 만들어져 있는 DB와 미리 연결을 맺은 Connection객체 빌려오기 
+			//요약 DB연결
+			con = ds.getConnection();
+			
+			//insert문장 완성하기
+			String sql = "INSERT INTO MEMBER (m_nickname, m_id, m_pw, m_name, m_email, m_date) " 
+									+ "values(   ?,        ?,    ?,    ?,      ?,      sysdate) ";
+                         
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getM_nickname());
+			pstmt.setString(2, vo.getM_id());
+			pstmt.setString(3, vo.getM_pw());
+			pstmt.setString(4, vo.getM_name());
+			pstmt.setString(5, vo.getM_email());
+			
+			
+			//PreparedStatement실행객체메모리에 설정된 insert전체 문장을 
+			//DB의 테이블에 실행!
+			pstmt.executeUpdate();
+						
+		} catch (Exception e) {
+			System.out.println("insertMember메소드 내부에서 SQL실행 오류 " + e);
+			e.printStackTrace();
+		} finally {
+			closeResource(); //자원해제
+		}
+		
+	}
+	
+	//입력한 아이디와 비밀번호를 매개변수로 받아  DB의 테이블에 저장되어 있는지 확인하는 메소드
+	public int loginCheck(String m_id, String m_pw) {
+		int check = -1;
+		
+		try {
+			//DB접속
+			con = ds.getConnection();
+			//매개변수로 로그인 아이디 받는 입력한 아이디에 해당되는 행을 조회 SELECT문
+			String sql = "select * from member where m_id=?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, m_id);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {//입력한 아이디로 조회한 행이 있으면?
+				//입력한 비밀번호와 조화된 비밀먼호와 비교 해서 있으면?
+				if(m_pw.equals(rs.getString("m_pw")) ) {
+					check = 1;
+				
+				}else { //아이디는 맞고 비번 틀림
+					check = 0;
+				}
+			}else {//아이디가 틀림
+			check = -1;
+			}
+		} catch (Exception e) {
+				System.out.println("loginCheck 메소드 내부에서 SQL문 실행 오류");
+				e.printStackTrace();	
+		}finally {
+			closeResource();
+		}
+		return check;
+		
+		}
+	
+
+	//아이디찾기
+		public MemberVO findId(String m_name, String m_email) {
+	
+		MemberVO vo = null;
+		
+		try {
+			
+			con = ds.getConnection();
+			
+			String sql = "select m_id from member where m_name=? and m_email=?";
+				pstmt = con.prepareStatement(sql);	
+				
+				pstmt.setString(1, m_name);
+				pstmt.setString(2, m_email);
+				
+				rs = pstmt.executeQuery();
+				
+				if (rs.next()) {
+					
+					vo = new MemberVO();
+					vo.setM_id(rs.getString("m_id"));
+					
+				}
+					
+		}catch (Exception e) {
+			
+			System.out.println("findId 메소드 내부에서 SQL문 실행 오류");
+			e.printStackTrace();
+			
+		}finally {
+			
+		}
+		
+		return vo;
+		
+	}
+		//비밀번호찾기
+		public MemberVO findPw(String m_name, String m_id, String m_email) {
+			
+			MemberVO vo = null;
+			
+			try {
+				
+				con = ds.getConnection();
+				
+				String sql = "select m_pw  from member where m_name=? and m_id=? and m_email=?";
+				
+					pstmt = con.prepareStatement(sql);	
+					
+					pstmt.setString(1, m_name);
+					pstmt.setString(2, m_id);
+					pstmt.setString(3, m_email);
+					
+					rs = pstmt.executeQuery();
+					
+					if (rs.next()) {
+						vo = new MemberVO();
+						vo.setM_pw(rs.getString("m_pw"));
+					
+					}
+			}catch (Exception e) {
+				
+				System.out.println("findPw 메소드 내부에서 SQL문 실행 오류");
+				e.printStackTrace();
+				
+			}finally {
+				
+			}
+			
+			return vo;
+			
+		}
+		
+		
+	
+		
+		
+	//회원 이메일을 이용해 회원 정보 조회
+	public MemberVO findMember(String m_id) {
+		
+			MemberVO vo = null;
+			
+			try {
+				//DB접속 : 커넥션풀에 만들어져 있는 커넥션 얻기
+				con = ds.getConnection();
+				
+				String sql = "SELECT * FROM member WHERE m_id='"+ m_id+"'";
+				
+				pstmt = con.prepareStatement(sql);
+				
+				rs = pstmt.executeQuery();
+				
+				if (rs.next()) {
+					
+					vo = new MemberVO();
+					vo.setM_id(m_id);
+					vo.setM_nickname("m_nickname");
+					vo.setM_pw(rs.getString("m_pw"));
+					vo.setM_name(rs.getString("m_name"));
+					vo.setM_email(rs.getString("m_email"));
+					
+					vo.setM_date(rs.getDate("m_date"));
+				}		
+			}catch(Exception e) {
+				System.out.println("getOneOrder메소드 에서  SQL오류 : " + e);
+			}finally {
+				closeResource(); //자원 해제
+			}
+			return vo;//조회된 차량 한대정보가 저장된 CarConfirmVo객체를 CarController서블릿으로 반환!
+	}
+	
+	//회원정보수정하는 메소드
+	//입력한 회원정보를 수정하는 메소드 
+		public int updateMember(HttpServletRequest request) {
+			
+			int result = 0; //수정성공시 1이 저장되고 , 수정 실패하면 0이 저장될 변수 
+			
+			try {
+				//커넥션 풀에서 DB와 미리 연결을 맺어 만들어져 있는 Connection객체 빌려오기 
+				//요약 : DB접속
+				con = ds.getConnection();
+				
+				String sql = "UPDATE member set"
+						   + " m_pw=?,"
+						   + " m_name=?,"
+						   + " m_email=?,"
+						   + " WHERE id=? ";
+				
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, request.getParameter("m_pw"));
+				pstmt.setString(2, request.getParameter("m_name"));
+				pstmt.setString(3, request.getParameter("m_email"));
+				pstmt.setString(9, request.getParameter("m_id"));
+				
+				result = pstmt.executeUpdate();
+				
+			} catch (Exception e) {
+				
+				System.out.println("updateMember메소드 내부에서 SQL실행 오류 ");
+				e.printStackTrace();
+				
+			} finally {
+				//자원해제
+				closeResource();
+			}
+			
+			return result;
+		}
+	
+	//회원정보삭제시키는 메소드 3.2 재봉
+	public int MemberDelete(String m_id, String m_pw) {
+		
+		int result = 0;
+		
+		try {
+			con = ds.getConnection();
+			
+			String sql = "delete from member where m_id=? and m_pw=?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, m_id);
+			pstmt.setString(2, m_pw);
+			
+			result = pstmt.executeUpdate();
+					
+		} catch (Exception e) {
+			System.out.println("MemberDelete메소드 내부에서 SQL실행 오류 ");
+			e.printStackTrace();
+		} finally {
+			//자원해제
+			closeResource();
+		}	
+		
+		return result;
+	}	
+
+}
+
